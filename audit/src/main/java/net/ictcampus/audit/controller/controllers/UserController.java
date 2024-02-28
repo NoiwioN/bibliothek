93f10b22f98bc540ac1d6cfed8d3c958d923b9bd
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-02-28 17:13:40 +0100

    CRUD für Benutzer

--- audit/src/main/java/net/ictcampus/audit/controller/controllers/UserController.java
+++ audit/src/main/java/net/ictcampus/audit/controller/controllers/UserController.java
@@ -9,9 +9,7 @@ import net.ictcampus.audit.controller.services.UserService;
 import net.ictcampus.audit.model.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.EntityNotFoundException;
@@ -26,6 +24,22 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping(path = "{id}")
+    @Operation(summary = "Zeigt einen Benutzer an")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = User.class))}),
+
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
+    public User findById(@PathVariable Integer id) {
+        try {
+            return userService.findById(id);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+    }
+
     @GetMapping
     @Operation(summary = "Gibt alle Benutzer zurück")
     @ApiResponses(value = {
@@ -38,6 +52,41 @@ public class UserController {
             return userService.findAll();
         } catch (EntityNotFoundException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+    }
+
+    @PutMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.NO_CONTENT) //TODO Swagger
+    public void update(@RequestBody User user) {
+        try {
+            userService.update(user);
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        }
+    }
+
+    @PostMapping(consumes = "application/json", path = "/sign-up")
+    @Operation(summary = "Erstellen eines Benutzers, kann auch missbraucht werden und einen vorhandenen Benutzer updaten")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "created"),
+            @ApiResponse(responseCode = "409", description = "Spezifische Fehlernachricht")
+    })
+    @ResponseStatus(HttpStatus.CREATED)
+    public void signUp(@RequestBody User user) {
+        try {
+            userService.signUp(user);
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        }
+    }
+
+    @DeleteMapping(path = "{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT) //TODO Swagger
+    public void deleteById(@PathVariable Integer id) {
+        try {
+            userService.deleteById(id);
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User was not found");
         }
     }
 }
--- audit/src/main/java/net/ictcampus/audit/controller/services/UserService.java
+++ audit/src/main/java/net/ictcampus/audit/controller/services/UserService.java
@@ -5,6 +5,9 @@ import net.ictcampus.audit.model.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -14,8 +17,35 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    public User findById(Integer id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseThrow(EntityNotFoundException::new);
+    }
+
     public Iterable<User> findAll() {
         return userRepository.findAll();
     }
+
+    public void update(User user) {
+        Iterable<User> users = findAll();
+        for (User u : users) {
+            if (u.getId().equals(user.getId())) {
+                //user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+                userRepository.save(user);
+                return;
+            }
+        }
+        throw new RuntimeException("User doesn't exist");
+    }
+
+    public void signUp(User user) {
+        //user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    public void deleteById(Integer id) {
+        userRepository.deleteById(id);
+    }
+
 
 }
