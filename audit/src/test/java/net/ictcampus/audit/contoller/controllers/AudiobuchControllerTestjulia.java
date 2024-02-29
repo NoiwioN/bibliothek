e28126e44aa0ccaafe6abd5b21de20faeba4f786
Julia Ebner <julia.ebner@ict-campus.net>
2024-02-29 15:05:09 +0100

    Test anfang

--- audit/src/test/java/net/ictcampus/audit/contoller/controllers/AudiobuchControllerTestjulia.java
+++ audit/src/test/java/net/ictcampus/audit/contoller/controllers/AudiobuchControllerTestjulia.java
@@ -1,17 +1,29 @@
 package net.ictcampus.audit.contoller.controllers;
 
+import net.ictcampus.audit.controller.controllers.AudiobuchController;
 import net.ictcampus.audit.controller.services.AudiobuchService;
 import net.ictcampus.audit.controller.services.UserDetailsServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.TimeZone;
 
+import static net.ictcampus.audit.utils.TestDataUtil.getTestAudiobuecher;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+@WebMvcTest(controllers = AudiobuchController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class AudiobuchControllerTestjulia {
-    private static final String JSON_ALL_MOVIES = "[" +
+    private static final String JSON_ALLE_AUDIOBUECHER = "[" +
             "{\"id\":1, \"name\": \"Audiobuch1\", \"duration\": \"50000\", \"genre\": { \"id\": 1, \"name\": \"Fantasy\"}}, " +
             "{\"id\":2, \"name\": \"Audiobuch2\", \"duration\": \"50000\", \"genre\": { \"id\": 1, \"name\": \"Fantasy\"}}, " +
             "{\"id\":3, \"name\": \"Audiobuch3\", \"duration\": \"50000\", \"genre\": { \"id\": 1, \"name\": \"Fantasy\"}}]";
@@ -38,8 +50,22 @@ public class AudiobuchControllerTestjulia {
     }
 
     /**
-     * GET-Request: Alle Filme werden herausgegeben und getestet, ob sie im richtigen JSON-Format
+     * GET-Request: Alle Audiobücher werden herausgegeben und getestet, ob sie im richtigen JSON-Format
      * geschickt werden
      * @throws Exception
      */
+    @Test
+    public void schaueGet_WennKeineParam_dannALLEAUDIOBUECHERZurueckgeben() throws Exception {
+        // gibt alle Audiobücher aus, sobald findAll im gemockten MovieService aufgerufen wird
+        doReturn(getTestAudiobuecher()).when(audiobuchService).findAll();
+
+        // GET-Request über localhost:8080/movies "geschickt"
+        mockMvc.perform(get("/audiobücher"))
+                // 200 (OK) wird erwartet -> bei erfolgreicher Abfrage, bekommen wir in der Regel
+                // den Statuscode 200 zurück
+                .andExpect(status().isOk())
+                // wir erwarten, dass der Inhalt der Abfrage mit unserem JSON-Gerüst (unsere oben
+                // definierte Konstante) übereinstimmt
+                .andExpect(content().json(JSON_ALLE_AUDIOBUECHER));
+    }
 }
