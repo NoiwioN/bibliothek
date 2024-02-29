c1b93a0ea155299977fcb962fe502d575533ad0c
Julia Ebner <julia.ebner@ict-campus.net>
2024-02-29 14:19:53 +0100

    Test vorarbeit fertig

--- audit/AudiobuchControllerTest.java
+++
@@ -1,6 +0,0 @@
-public class AudiobuchControllerTest {
-    private static final String JSON_ALL_MOVIES = "[" +
-            "{\"id\":1, \"name\": \"Harry Potter and the Philosopher's Stone\", \"duration\": \"7:30:00\", \"genre\": { \"id\": 1, \"name\": \"Fantasy\"}}, " +
-            "{\"id\":2, \"name\": \"The Hobbit\", \"duration\": \"9:15:00\", \"genre\": { \"id\": 1, \"name\": \"Fantasy\"}}, " +
-            "{\"id\":3, \"name\": \"1984\", \"duration\": \"8:45:00\", \"genre\": { \"id\": 2, \"name\": \"Science Fiction\"}}]";
-}
