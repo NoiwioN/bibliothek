a999c5e7c2d1212ceed60e187325bc04c0ee8fde
Julia Ebner <julia.ebner@ict-campus.net>
2024-02-29 09:39:28 +0100

    Audiobuch fertig

---
+++ audit/src/main/java/net/ictcampus/audit/controller/repositories/AudiobuchRepository.java
@@ -0,0 +1,7 @@
+package net.ictcampus.audit.controller.repositories;
+
+import net.ictcampus.audit.model.models.Audiobuch;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AudiobuchRepository extends CrudRepository<Audiobuch, Integer> {
+}
