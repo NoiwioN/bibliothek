2e0416ceac8614c36ff6efbbfd3f32cbf132cd8c
NoiwioN <mattias.zurbuchen@ict-campus.net>
2024-02-29 11:49:31 +0100

    Ausleihe Swagger

--- audit/src/main/java/net/ictcampus/audit/controller/controllers/AusleiheController.java
+++ audit/src/main/java/net/ictcampus/audit/controller/controllers/AusleiheController.java
@@ -1,63 +1,104 @@
 package net.ictcampus.audit.controller.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import net.ictcampus.audit.controller.services.AusleiheService;
 import net.ictcampus.audit.model.models.Ausleihe;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.EntityNotFoundException;
+import javax.validation.Valid;
 
 @RestController
-@RequestMapping(path="/ausleihen")
+@RequestMapping(path = "/ausleihen")
 public class AusleiheController {
     private final AusleiheService ausleiheService;
+
     @Autowired
 
     public AusleiheController(AusleiheService ausleiheService) {
         this.ausleiheService = ausleiheService;
     }
+
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public Iterable<Ausleihe> findAll(){
-        try{
+    @Operation(summary = "Gibt alle Ausleihen zurück")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ausleihen wurden gefunden", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Ausleihe.class))}),
+            @ApiResponse(responseCode = "404", description = "Ausleihen wurden nicht gefunden"),
+            @ApiResponse(responseCode = "403", description = "Nicht autorisiert")
+    })
+    public Iterable<Ausleihe> findAll() {
+        try {
             return ausleiheService.findAll();
-        }catch (EntityNotFoundException e){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Es konnten keine Ausleihen gefunden werden");
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Ausleihen wurden nicht gefunden");
         }
     }
 
     @GetMapping(path = "{id}")
-    public Ausleihe findById(@PathVariable Integer id){
-        try{
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Gibt eine bestimmte Ausleihe zurück")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ausleihe wurde gefunden", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Ausleihe.class))}),
+            @ApiResponse(responseCode = "404", description = "Ausleihe wurde nicht gefunden"),
+            @ApiResponse(responseCode = "403", description = "Nicht autorisiert")
+    })
+    public Ausleihe findById(@PathVariable Integer id) {
+        try {
             return ausleiheService.findById(id);
-        }catch(EntityNotFoundException e){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Diese Ausleihe wurde nicht gefunden");
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Ausleihe wurde nicht gefunden");
         }
     }
 
     @PostMapping(consumes = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
-    public void insert(@RequestBody Ausleihe ausleihe){
+    @Operation(summary = "Erstellt eine neue Ausleihe")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Ausleihe wurde erstellt"),
+            @ApiResponse(responseCode = "409", description = "Ausleihe konnte nicht erstellt werden"),
+            @ApiResponse(responseCode = "403", description = "Nicht autorisiert")
+    })
+    public void insert(@Valid @RequestBody Ausleihe ausleihe) {
         try {
             ausleiheService.insert(ausleihe);
-        } catch (RuntimeException e){
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "Ausleihe konnte nicht erfasst werden");
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Ausleihe konnte nicht erstellt werden");
         }
     }
+
     @PutMapping(consumes = "application/json")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update(@RequestBody Ausleihe ausleihe){
+    @Operation(summary = "Aktualisiert eine Ausleihe")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Ausleihe wurde aktualisiert"),
+            @ApiResponse(responseCode = "409", description = "Ausleihe konnte nicht aktualisiert werden"),
+            @ApiResponse(responseCode = "403", description = "Nicht autorisiert")
+    })
+
+    public void update(@Valid @RequestBody Ausleihe ausleihe) {
         try {
             ausleiheService.update(ausleihe);
-        } catch (RuntimeException e){
+        } catch (RuntimeException e) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Ausleihe konnte nicht aktualisiert werden");
         }
     }
-    @DeleteMapping(path="{id}")
+
+    @DeleteMapping(path = "{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Löschen einer Ausleihe")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Ausleihe wurde gelöscht"),
+            @ApiResponse(responseCode = "404", description = "Ausleihe wurde nicht gefunden"),
+            @ApiResponse(responseCode = "403", description = "Nicht autorisiert")
+    })
     public void deleteById(@PathVariable Integer id) {
         try {
             ausleiheService.deleteById(id);
--- audit/src/main/java/net/ictcampus/audit/controller/controllers/UserController.java
+++ audit/src/main/java/net/ictcampus/audit/controller/controllers/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.EntityNotFoundException;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(path = "users")
@@ -26,82 +27,83 @@ public class UserController {
 
     @GetMapping(path = "{id}")
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Returns a specific User")
+    @Operation(summary = "Gibt einen bestimmten Benutzer zurück")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Success",
-                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = User.class))}),
-
-            @ApiResponse(responseCode = "404", description = "User not found"),
-            @ApiResponse(responseCode = "403", description = "Forbidden")
+            @ApiResponse(responseCode = "404", description = "Benutzer wurde nicht gefunden"),
+            @ApiResponse(responseCode = "403", description = "Nicht autorisiert"),
+            @ApiResponse(responseCode = "200", description = "Benutzer wurde gefunden",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = User.class))})
     })
     public User findById(@PathVariable Integer id) {
         try {
             return userService.findById(id);
         } catch (EntityNotFoundException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Benutzer wurde nicht gefunden");
         }
     }
 
     @GetMapping
-    @Operation(summary = "Returns all Users")
+    @Operation(summary = "Gibt alle Benutzer zurück")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Success",
+            @ApiResponse(responseCode = "200", description = "Benutzer wurden gefunden",
                     content = {@Content(mediaType = "application/json", schema = @Schema(implementation = User.class))}),
-            @ApiResponse(responseCode = "404", description = "Users not found"),
-            @ApiResponse(responseCode = "403", description = "Forbidden")
+            @ApiResponse(responseCode = "404", description = "Benutzer konnten nicht gefunden werden"),
+            @ApiResponse(responseCode = "403", description = "Nicht autorisiert")
     })
     public Iterable<User> findAll() {
         try {
             return userService.findAll();
         } catch (EntityNotFoundException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Benutzer konnten nicht gefunden werden");
         }
     }
 
     @PutMapping(consumes = "application/json")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @Operation(summary = "Returns all Users")
+    @Operation(summary = "Aktualisiert einen Benutzer")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "No Content"),
-            @ApiResponse(responseCode = "404", description = "Users not found"),
-            @ApiResponse(responseCode = "403", description = "Forbidden")
+            @ApiResponse(responseCode = "204", description = "Der Benutzer konnte aktualisiert werden"),
+            @ApiResponse(responseCode = "404", description = "Benutzer nicht gefunden"),
+            @ApiResponse(responseCode = "403", description = "Nicht autorisiert"),
+            @ApiResponse(responseCode = "409", description = "Der Benutzer konnte nicht aktualisiert werden")
     })
-    public void update(@RequestBody User user) {
+    public void update(@Valid @RequestBody User user) {
         try {
             userService.update(user);
         } catch (RuntimeException e) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "User could not be updated");
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Der Benutzer konnte nicht aktualisiert werden");
         }
     }
 
     @PostMapping(consumes = "application/json", path = "/sign-up")
-    @Operation(summary = "Creates a new User")
+    @Operation(summary = "Erstellt einen neuen Benutzer")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "created"),
-            @ApiResponse(responseCode = "409", description = "User could not be created")
+            @ApiResponse(responseCode = "201", description = "Benutzer wurde erstellt"),
+            @ApiResponse(responseCode = "403", description = "Nicht autorisiert"),
+            @ApiResponse(responseCode = "409", description = "Benutzer konnte nicht erstellt werden")
     })
     @ResponseStatus(HttpStatus.CREATED)
-    public void signUp(@RequestBody User user) {
+    public void signUp(@Valid @RequestBody User user) {
         try {
             userService.signUp(user);
         } catch (RuntimeException e) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "User could not be created");
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Benutzer konnte nicht erstellt werden");
         }
     }
 
     @DeleteMapping(path = "{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @Operation(summary = "Deletes a User")
+    @Operation(summary = "Löscht einen Benutzer")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "No Content"),
-            @ApiResponse(responseCode = "404", description = "Users not found"),
-            @ApiResponse(responseCode = "403", description = "Forbidden")
+            @ApiResponse(responseCode = "204", description = "Benutzer wurde gelöscht"),
+            @ApiResponse(responseCode = "404", description = "Benutzer wurde nicht gefunden"),
+            @ApiResponse(responseCode = "403", description = "Nicht autorisiert")
     })
     public void deleteById(@PathVariable Integer id) {
         try {
             userService.deleteById(id);
         } catch (RuntimeException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Benutzer wurde nicht gefunden");
         }
     }
 }
